b2bd7c1548b4b42d0f99a064a6a6bc92181c8995
kstenbeck <49898571+kstenbeck@users.noreply.github.com>
2020-05-26 11:44:49 +0200

    add

---
+++ webpush-service-worker.js
@@ -0,0 +1,3 @@
+importScripts('https://api.pushio.com/webpush/sdk/service_min.js');
+var config = '{"appserviceKey":"BLAqWYeaCNhBqHkwvGzxZAQSdWcN8xjgkhK_UOdBuG3fpVzfb0UQZjfUFM5i0qwO35XRiWVnFpFCuS7wZ2KGYKA=","apiKey":"ABEpKg2KWcSJkibBbisczOdo4","accountToken":"ABEiG619VR_I2X614Nr6-nSRg","appver":"0.0.0","apiHost":"https://api.pushio.com","lazy":false}';
+orawp.init(config);
